d5f5aefe9e7de28db0691b4259cd110d3883d1b4
lpgoulart <leonardo@roffor.com.br>
2019-10-15 14:55:21 -0300

    update name HelloWorld to GETRequest

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GETRequest />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GETRequest from './components/GETRequest.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    GETRequest
   }
 }
 </script>
---
+++ src/components/GETRequest.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>
+      GET Request from Json Placeholder
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GETRequest'
+}
+</script>
+
+<style scoped>
+
+</style>
