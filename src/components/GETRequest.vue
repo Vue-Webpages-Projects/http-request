88f7eb70f7aec0907e1177c07f0c9e38e8b74c77
lpgoulart <leonardo@roffor.com.br>
2019-10-15 15:04:32 -0300

    http get request from jsonplaceholder

--- src/components/GETRequest.vue
+++ src/components/GETRequest.vue
@@ -3,15 +3,55 @@
     <h2>
       GET Request from Json Placeholder
     </h2>
+
+    <div v-for="post in posts"
+                :key="post.id"
+                class="posts">
+
+      <div class="post">
+        <h4>
+          {{post.title}}
+        </h4>
+
+        <article>
+          {{post.body}}
+        </article>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'GETRequest'
+  name: 'GETRequest',
+  data() {
+    return {
+      posts: []
+    }
+  },
+  created() {
+    this.$http.get('https://jsonplaceholder.typicode.com/posts')
+      .then(function(data){
+        this.posts = data.body.slice(0,10);
+      })
+  }
+
 }
 </script>
 
 <style scoped>
-
+  .posts {
+    text-align: justify;
+    width: 100%;
+    margin-top: 20px;
+  }
+  .post {
+    max-width: 800px;
+    margin: auto;
+    padding: 5px 15px 15px 15px;
+    background-color: #eee;
+    border-radius: 10px;
+  }
 </style>
